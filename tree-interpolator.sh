9e921ef22b7e24c73714a6bb79cfd74a29a030c8
Stephen Mather <1174901+smathermather@users.noreply.github.com>
2026-05-28 12:19:33 -0400

    Remove lastools

--- tree-interpolator.sh
+++ tree-interpolator.sh
@@ -1,40 +1,103 @@
 #!/bin/sh
 
-# lasmerge -i N2170660.las  -i N2175660.las  -i N2175665.las  -i N2180665.las -o west.las
-# merge your las files first...
-
-# Calculate Tree height using lasheight--
+# Calculate Tree height using pdal pipeline
 # replace the z value with a height value
 # allows for floating point z value
 
-echo "		 								Calculating Tree Height..."
-lasheight.exe -i $1 -replace_z -o $2.las
+# -----------------------------------------------------
+# Tree height calculations
+# 
+# Start with las or laz files
+# End with text file with X, Y, and Tree Height
+# -----------------------------------------------------
+
+#[
+#    "input.las",
+#    {
+#        "type":"filters.hag_delaunay"
+#    },
+#    {
+#        "type":"writers.las",
+#        "filename":"tree_height.laz",
+#        "extra_dims":"HeightAboveGround=float32"
+#    }
+#]
+echo "	Calculating Tree Height..."
+pdal pipeline height.json
+
 
 # Eliminate all but tall vegetation (class 5)
 
+#[
+#    "tree_height.laz",
+#    {
+#                "type":"filters.expression",
+#                "expression":"((HeightAboveGround > 0) && Classification == 5)"
+#    },
+#    {
+#        "type":"writers.las",
+#        "filename":"tree_height_only.laz",
+#        "extra_dims":"HeightAboveGround=float32"
+#    }
+#]
+
 echo "		 								Eliminating non-veg points..."
-#las2las -i $2.las -eliminate_class 2 -o $2.out.las
-#las2las -i $2.out.las -eliminate_class 1 -o $2.out.out.las
+pdal pipeline class.json
 
-las2las -i $2.las -eliminate_class 1 2 -o $2.out.out.las
+#[
+#    {
+#        "type":"readers.las",
+#        "filename":"tree_height_only.laz"
+#    },
+#    {
+#        "type":"writers.text",
+#        "format":"csv",
+#        "order":"X,Y,HeightAboveGround",
+#        "keep_unspecified":"false",
+#        "delimiter":" ",
+#        "filename":"tree_height_only.txt",
+#        "write_header":"false"
+#    }
+#]
 
-las2txt $2.out.out.las $2.txt
-# Count how many points we have from the las file
-# (Could this be extracted from Standard Error from last step?)
-numlines=`more $2.txt | grep -v ' 0' | ~/Documents/CM/Canopy/./linecount.o`
+echo "
+  Write out text file..."
+pdal pipeline text.json
+
+# -----------------------------------------------------
+# Prepare Include Files for Povray
+# -----------------------------------------------------
+
+# Count number of tree height points
+numlines=`more tree_height_only.txt | wc -l`
 echo " 										$numlines points in resultant las."
 
 # Build include files of tree locations and heights for PovRay rendering
-echo "#declare tree_coords = array["$numlines"]{" > $2_coords.inc
-more $2.txt | grep -v ' 0' | awk '{print "<" $1 ", 0, " $2 ">" };' >> $2_coords.inc
-echo "} #declare tree_height = array["$numlines"]{" >> $2_coords.inc
-more $2.txt | grep -v ' 0' |  awk '{ print $3 "," };' >> $2_coords.inc
-echo '}' >> $2_coords.inc
+# Tree locations are fixed at height of 0
 
-# Calculate extent of point cloud using lasboundary and ogrinfo
-lasboundary.exe -convex -i $2.out.out.las -o $2.out.shp
+# Write tree coordinate array header
+echo "#declare tree_coords = array["$numlines"]{" > tree_height_coords.inc
 
-bb=`ogrinfo -ro -al -so -geom=NO $2.out.shp | grep Extent | tr '(' ' ' | tr ')' ' ' | tr ',' ' '`
+# Write X, 0, Y locations
+more tree_height_only.txt | grep -v ' 0' | awk '{print "<" $1 ", 0, " $2 ">" };' >> tree_height_coords.inc
+
+# Write tree height array header
+echo "} #declare tree_height = array["$numlines"]{" >> tree_height_coords.inc
+
+# Write tree heights
+more tree_height_only.txt | grep -v ' 0' |  awk '{ print $3 "," };' >> tree_height_coords.inc
+echo '}' >> tree_height_coords.inc
+
+# -----------------------------------------------------
+# Prepare Scene Extent Variables for Povray
+# using pdal tindex and ogrinfo
+# -----------------------------------------------------
+
+# Calculate extent of point cloud using pdal tindex
+pdal tindex create --tindex tree_height_boundary.sqlite --filespec tree_height_only.laz -f SQLite
+
+# From index file we'll calculate scene extent and center
+bb=`ogrinfo -ro -al -so -geom=NO tree_height_boundary.sqlite | grep Extent | tr '(' ' ' | tr ')' ' ' | tr ',' ' '`
 minx=`echo $bb | awk '{print $2};'`
 miny=`echo $bb | awk '{print $3};'`
 maxx=`echo $bb | awk '{print $5};'`
@@ -48,35 +111,42 @@ centerx=`echo "($maxx + $minx) / 2" | bc`
 centery=`echo "($maxy + $miny) / 2" | bc`
 
 # Set camera size, pixel size and image size 
-camera_size=$3
-pixel_size=$4
+camera_size=5000
+pixel_size=1
 image_size=`echo "scale=20; $camera_size / $pixel_size" | bc`
 
 ulx=`echo "scale=20; $centerx - $camera_size / 2" | bc`
 uly=`echo "scale=20; $centery + $camera_size / 2" | bc`
 
+# -----------------------------------------------------
+# Write the pov file for rendering
+# -----------------------------------------------------
+
 # Using what we know, let's build the pov file for rendering
-echo "//Set the center of image" > $2.pov
-echo "\n" >> $2.pov
-echo "#version 3.6;" >> $2.pov
-echo "#declare scene_center_x=$centerx;" >> $2.pov
-echo "#declare scene_center_y=$centery;" >> $2.pov
+echo "//Set the center of image" > render.pov
+echo "\n" >> render.pov
+echo "#version 3.6;" >> render.pov
+echo "#declare scene_center_x=$centerx;" >> render.pov
+echo "#declare scene_center_y=$centery;" >> render.pov
 
-echo "//Include locations and heights for trees." >> $2.pov
-echo '#include "'$2'_coords.inc"' >> $2.pov
+echo "//Include locations and heights for trees." >> render.pov
+echo '#include "tree_height_coords.inc"' >> render.pov
 
-echo "#declare Camera_Size = $camera_size;" >> $2.pov
+echo "#declare Camera_Size = $camera_size;" >> render.pov
 
-more treepov.inc >> $2.pov
+more treepov.inc >> render.pov
 
-# And render...
-povray +I$2.pov +O$2.png +FN16 +W$image_size +H$image_size +A +D
+# -----------------------------------------------------
+# Time to render our scene!
+# -----------------------------------------------------
+
+povray +Irender.pov +Orender.png +FN16 +W$image_size +H$image_size +A +D
 
 # Now time to create a world file to georeference
 
-echo $pixel_size > $2.pgw
-echo 0 >> $2.pgw
-echo 0 >> $2.pgw
-echo -$pixel_size >> $2.pgw
-echo $ulx >> $2.pgw
-echo $uly >> $2.pgw
+echo $pixel_size > render.pgw
+echo 0 >> render.pgw
+echo 0 >> render.pgw
+echo -$pixel_size >> render.pgw
+echo $ulx >> render.pgw
+echo $uly >> render.pgw
